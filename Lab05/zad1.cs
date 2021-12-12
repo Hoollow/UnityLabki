f628822cb6ed6f980a60dcc05e5f6b84440f9f67
Hoollow <mateuszkowalewski12@gmail.com>
2021-12-12 17:38:29 +0100

    Add files via upload

---
+++ Lab05/zad1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad1 : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private bool move = true;
+    private bool moveWithPlayer = true;
+    public Vector3 startPostion;
+    public Vector3 endPositon;
+    
+
+    private void Start()
+    {
+        startPostion = transform.position;
+    }
+
+    private void Update()
+    {
+        if(moveWithPlayer)
+        {
+            if (move)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPositon, speed * Time.deltaTime);
+            }
+            else
+                transform.position = Vector3.MoveTowards(transform.position, startPostion, speed * Time.deltaTime);
+
+            if(transform.position == endPositon)
+            {
+                move = false;
+            }
+            else if(transform.position == startPostion)
+            {
+                move = true;
+            }
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            moveWithPlayer = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            moveWithPlayer = false;
+        }
+    }
+
+}
---
+++ Lab05/zad2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad2 : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private bool move = true;
+    private bool moveWithPlayer = true;
+    public Vector3 startPostion;
+    public Vector3 endPositon;
+
+
+    private void Start()
+    {
+        startPostion = transform.position;
+    }
+
+    private void Update()
+    {
+        if (moveWithPlayer)
+        {
+            if (move)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPositon, speed * Time.deltaTime);
+            }
+            else
+                transform.position = Vector3.MoveTowards(transform.position, startPostion, speed * Time.deltaTime);
+
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            moveWithPlayer = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            moveWithPlayer = false;
+        }
+    }
+}
